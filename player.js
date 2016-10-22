e8a332dfa3e9a5f3391a2a2cd028eb533e73570e
Ahmed <10769951+holazt@users.noreply.github.com>
2016-10-22 17:23:17 +0800

    update

---
+++ player.js
@@ -0,0 +1,33 @@
+var ap5 = new APlayer({
+    element: document.getElementById('player5'),
+    narrow: false,
+    autoplay: false,
+ //   showlrc: 3,
+    mutex: true,
+    theme: '#ad7a86',
+    mode: 'random',
+    listmaxheight: '240px',
+    music: [
+        {
+            title: 'The truth that you leave',
+            author: 'Pianoboy',
+            url: './sound/Pianoboy - The truth that you leave.mp3',
+          //  pic: 'http://devtest.qiniudn.com/あっちゅ～ま青春!.jpg',
+          //  lrc: 'あっちゅ～ま青春!.lrc'
+        },
+        {
+            title: 'Man At Arms',
+            author: 'Position Music',
+            url: './sound/Position Music - Man At Arms.mp3',
+         //   pic: 'http://devtest.qiniudn.com/secret base~.jpg',
+         //   lrc: 'secret base~君がくれたもの~.lrc'
+        },
+        {
+            title: '青い宝',
+            author: 'やなわらばー',
+            url: './sound/やなわらばー - 青い宝.mp3',
+          //  pic: 'http://devtest.qiniudn.com/回レ！雪月花.jpg',
+          //  lrc: '回レ！雪月花.lrc'
+        }
+    ]
+});
